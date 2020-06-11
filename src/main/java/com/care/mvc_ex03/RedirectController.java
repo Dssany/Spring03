e3eb845c74ddec2f0cb229b3df88db0760b642f1
bin <tjdqls45685210@gmail.com>
2020-06-11 10:27:51 +0900

    redirectController 작업중...

--- src/main/java/com/care/mvc_ex03/RedirectController.java
+++ src/main/java/com/care/mvc_ex03/RedirectController.java
@@ -1,5 +1,14 @@
 package com.care.mvc_ex03;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RedirectController {
 
+	@RequestMapping("re/index")
+	public String index() {
+		return "redirect01/index";
+	}
+	
 }
